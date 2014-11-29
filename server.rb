edb30999cb9d53455f13a2af2b5adc40eb4e75ed
David Coleman <colemandavid55@gmail.com>
2014-11-28 21:03:39 -0600

    add get/post requests that affect same url path

--- server.rb
+++ server.rb
@@ -7,3 +7,21 @@ get '/welcome/:name' do
   @name = params[:name]
   erb :welcome
 end
+
+get '/say/*/to/*' do
+  puts params
+  erb :say
+end
+
+get '/test' do
+  erb :info
+end
+
+post '/test' do 
+  puts params
+  @name = params[:name]
+  @email = params[:mail]
+  @vegetarian = params[:vegetarian]
+  @blood_type = params[:bloodtype]
+  erb :summary
+end
